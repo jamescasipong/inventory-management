b9063522a39ce70b5bbc60a0eae8182f54f89e3f
Arisuuuuu <jamesxcasipong@gmail.com>
2024-09-03 09:46:26 +0800

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,7 @@ const Login = ({setChange}) => {
     <div class="bg-gray-50 font-[sans-serif]">
       <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4">
         <div class="max-w-md w-full">
-          <a href="javascript:void(0)"><img
-            src="https://readymadeui.com/readymadeui.svg" alt="logo" class='w-40 mb-8 mx-auto block' />
-          </a>
+          
 
           <div class="p-8 rounded-2xl bg-white shadow">
             <h2 class="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
