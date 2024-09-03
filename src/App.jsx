84cf36c97179577fdaa443a8d2fa3cce477802ed
Arisuuuuu <jamesxcasipong@gmail.com>
2024-09-03 09:44:52 +0800

    Add Login and Signup
    Wala pang routing pero nilagyan ko ng function na ihahide at ishoshow yung login at register.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+
+
 
 function App() {
+  const [signIn, change] = useState(true);
+  
   return (
     <>
-      <p>isa kang malaking burat</p>
-      <div>1 week development</div>
+    <section id="login" className={`${signIn ? "" : "hidden"}`}>
+      <Login setChange={()=> change(signIn => !signIn)}></Login>
+    </section>
+    <section id="signup" className={`${signIn ? "hidden": "flex"}`}>
+      <Signup setChange={()=> change(signIn => !signIn)}></Signup>
+    </section>
     </>
   );
 }
